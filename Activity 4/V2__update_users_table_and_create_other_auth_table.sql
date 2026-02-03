149fb5787fc480c907f39570dd2c98283f09028a
Keyt15 <katemergelaine.lacanlali@lorma.edu>
2026-02-03 15:30:57 +0800

    activity 4

---
+++ Activity 4/V2__update_users_table_and_create_other_auth_table.sql
@@ -0,0 +1,18 @@
+alter table users
+add column password varchar(60) not null,
+add column date_created timestamp default now();
+
+
+create table roles(
+    id SERIAL primary key,
+    name varchar(50) unique not null,
+    date_created timestamp default now()
+);
+
+create table user_roles(
+    id SERIAL primary key,
+    user_id int not null,
+    role_id int not null,
+    foreign key(user_id) references users(id) on update cascade on delete cascade,
+    foreign key(role_id) references roles(id)on update cascade on delete cascade
+);
